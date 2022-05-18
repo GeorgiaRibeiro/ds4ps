c5b243191089d2463bd4e8cf7ca008d74636244e
Georgia Ribeiro <georgiamribeiro@gmail.com>
2022-05-18 18:23:23 -0300

    upload seminario path

---
+++ seminario-ds4ps-cap5/pacotes-e-trackdown.R
@@ -0,0 +1,14 @@
+# Title: Base Class Packages
+# Authors:  Davi Moreira
+# Corresponding authors:  davi.moreira(at)ufpe( dot)br; 
+# Date:  2022-04-30
+# Description: Install all packages for the class
+
+# rm(list = ls())
+
+# packages ---------------------------------------------------------------------
+install.packages("rmarkdown")
+install.packages("xaringan")
+install.packages("xaringanthemer", dependencies = TRUE)
+install.packages("pagedown")
+install.packages("DT")

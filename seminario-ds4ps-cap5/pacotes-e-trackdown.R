f959a3a22e92df6945a4214a67f134c5424a3e33
Georgia Ribeiro <georgiamribeiro@gmail.com>
2022-05-20 16:42:24 -0300

    update

--- seminario-ds4ps-cap5/pacotes-e-trackdown.R
+++ seminario-ds4ps-cap5/pacotes-e-trackdown.R
@@ -1,8 +1,7 @@
-# Title: Base Class Packages
-# Authors:  Davi Moreira
-# Corresponding authors:  davi.moreira(at)ufpe( dot)br; 
-# Date:  2022-04-30
-# Description: Install all packages for the class
+# Title: Base Project Packages
+# Authors:  Alexsandra. Georgia e Mayres
+# Date:  2022-05-20
+# Description: Install all packages for the project and use collaborative package
 
 # rm(list = ls())
 
@@ -12,3 +11,13 @@ install.packages("xaringan")
 install.packages("xaringanthemer", dependencies = TRUE)
 install.packages("pagedown")
 install.packages("DT")
+install.packages("here")
+install.packages("trackdown")
+
+#editar colaborativamente ---------
+ 
+trackdown::upload_file(here("apresentacao", "apresentacao.Rmd")) #so a primeira vez
+
+trackdown::download_file(here("apresentacao", "apresentacao.Rmd")) #baixar arquivo depois de editar no drive.
+
+trackdown::update_file(here("apresentacao", "apresentacao.Rmd")) #atualizar arquivo do drive com edições do .Rmd
